adae69d4004e3414d0d1de8352331e0a3113cccf
BrenoBarbalho <brenotba@gmail.com>
2023-01-26 21:25:06 -0300

    Atualizando aula 3

--- Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
@@ -21,21 +21,45 @@ namespace Tarefas.Web.Controllers
         
         public IActionResult Details(int id)
         {
-            var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+            //var tarefa = listaDeTarefas.Find(tarefa => tarefa.Id == id);
+            //return View(tarefa);
+            var tarefaDAO = new TarefaDAO();
+            var tarefaDTO = tarefaDAO.Consultar(id);
+
+            var tarefa = new Tarefa(){
+                Id = tarefaDTO.Id,
+                    Titulo = tarefaDTO.Titulo,
+                    Descricao = tarefaDTO.Descricao,
+                    Concluida =tarefaDTO.Concluida
+            };
             return View(tarefa);
         }
 
         public IActionResult Index()
         {            
+            var tarefaDAO = new TarefaDAO();
+            var listaDeTarefasDTO = tarefaDAO.Consultar();
+
+            var listaDeTarefas = new List<Tarefa>();
+
+            foreach (var tarefaDTO in listaDeTarefasDTO)
+            {
+                listaDeTarefas.Add(new Tarefa(){
+                    Id = tarefaDTO.Id,
+                    Titulo = tarefaDTO.Titulo,
+                    Descricao = tarefaDTO.Descricao,
+                    Concluida =tarefaDTO.Concluida
+                });
+            }
             return View(listaDeTarefas);
         }
 
         public IActionResult Create()
         {
-            return View();
+            return RedirectToAction("Index");
         }
 
-        [HttpPost]
+        [HttpPost]  
         public IActionResult Create(Tarefa tarefa)
         {
             var tarefaDTO = new TarefaDTO 
@@ -50,5 +74,43 @@ namespace Tarefas.Web.Controllers
 
             return View();
         }
+
+        public IActionResult Update(Tarefa tarefa){
+            var tarefaDTO = new TarefaDTO {
+                Id = tarefa.Id,
+                Titulo = tarefa.Titulo,
+                Descricao = tarefa.Descricao,
+                Concluida = tarefa.Concluida
+            };
+            var tarefaDAO = new TarefaDAO();
+            tarefaDAO.Atualizar(tarefaDTO);
+
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Update(int id) 
+        {
+            var tarefaDAO = new TarefaDAO();
+            var tarefaDTO = tarefaDAO.Consultar(id);
+  
+            var tarefa = new Tarefa(){
+                Id = tarefaDTO.Id,
+                    Titulo = tarefaDTO.Titulo,
+                    Descricao = tarefaDTO.Descricao,
+                    Concluida =tarefaDTO.Concluida
+
+            };
+
+            return View(tarefa);
+        }
+
+        public IActionResult Delete (int id){
+            var tarefaDAO = new TarefaDAO();
+            tarefaDAO.Excluir(id);
+
+            return RedirectToAction("Index");
+        }
+        
     }
 }
